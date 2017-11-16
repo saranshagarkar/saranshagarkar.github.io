4545ee0c838ae9b7bed1eb566733b5676c59daaa
Saransh <saranshagarkar@gmail.com>
2017-11-16 16:22:48 +0530

    Save work

--- metaptest.bat
+++ metaptest.bat
@@ -1,6 +1,6 @@
 F:
 cd Data/Saransh_projects/non-school/miscellaneous/comp_saransh/comp_programs/command line tryouts
 git add .
-git commit -m "Save"
+git commit -m "Save work"
 git push -u origin master
 
